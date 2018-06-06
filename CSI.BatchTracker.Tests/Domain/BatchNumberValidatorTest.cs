0eb28f45d4dcc243e8c080299103fed6deff630e
Jason L. Walker <jasonlanewalker@gmail.com>
2018-06-06 10:55:55 -0500

    created first test using nUnit

---
+++ CSI.BatchTracker.Tests/Domain/BatchNumberValidatorTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using CSI.BatchTracker.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.Domain
+{
+    [TestFixture]
+    public class BatchNumberValidatorTest
+    {
+        [Test]
+        public void ValidBatchNumber()
+        {
+            BatchNumberValidator validator = new BatchNumberValidator();
+            Assert.True(validator.Validate("872880407301"));
+        }
+    }
+}
---
+++ CSI.BatchTracker/Domain/BatchNumberValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Domain
+{
+    public class BatchNumberValidator
+    {
+        public BatchNumberValidator()
+        {
+
+        }
+
+        public bool Validate(string batchNumber)
+        {
+            return true;
+        }
+    }
+}
