f59e52314442e7a3fd8e32d604facb16f41619d8
Jason L. Walker <jasonlanewalker@gmail.com>
2018-06-06 14:47:28 -0500

    created DispenseOperator native model

---
+++ CSI.BatchTracker.Tests/Domain/NativeModels/DispenseOperatorTest.cs
@@ -0,0 +1,51 @@
+﻿using CSI.BatchTracker.Domain.NativeModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.Domain.NativeModels
+{
+    [TestFixture]
+    public class DispenseOperatorTest
+    {
+        DispenseOperator dispenseOperator;
+
+        [Test]
+        public void OperatorIsSetupCorrectly()
+        {
+            string firstName = "Jane";
+            string lastName = "Doe";
+            dispenseOperator = new DispenseOperator(firstName, lastName);
+
+            Assert.AreEqual(firstName, dispenseOperator.FirstName);
+            Assert.AreEqual(lastName, dispenseOperator.LastName);
+        }
+
+        [Test]
+        public void GetOperatorInitials()
+        {
+            string firstName = "Jane";
+            string lastName = "Doe";
+            string initials = "JD";
+
+            dispenseOperator = new DispenseOperator(firstName, lastName);
+
+            Assert.AreEqual(initials, dispenseOperator.GetInitials());
+        }
+
+        [Test]
+        public void ExceptionIfOperatorFirstNameIsEmpty()
+        {
+            Assert.Throws<System.ArgumentException>(() => new DispenseOperator("", "Doe"));
+        }
+
+        [Test]
+        public void ExceptionIfOperatorLastNameIsEmpty()
+        {
+            Assert.Throws<System.ArgumentException>(() => new DispenseOperator("Jane", ""));
+        }
+    }
+}
---
+++ CSI.BatchTracker/Domain/NativeModels/DispenseOperator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Domain.NativeModels
+{
+    public class DispenseOperator
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public DispenseOperator(string firstName, string lastName)
+        {
+            CheckIfNameIsEmpty(firstName);
+            CheckIfNameIsEmpty(lastName);
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        void CheckIfNameIsEmpty(string name)
+        {
+            if (name.Length == 0)
+            {
+                throw new ArgumentException("Dispense Operator name field(s) cannot be empty.");
+            }
+        }
+
+        public string GetInitials()
+        {
+            return string.Format("{0}{1}", FirstName.Substring(0, 1), LastName.Substring(0, 1));
+        }
+    }
+}
