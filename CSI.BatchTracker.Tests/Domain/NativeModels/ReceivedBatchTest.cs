d8522c1d6dedabb37a05bbad2dbdaffc5ae8779a
Jason L. Walker <jasonlanewalker@gmail.com>
2018-06-08 10:24:15 -0500

    added po number to ReceivedBatch model

--- CSI.BatchTracker.Tests/Domain/NativeModels/ReceivedBatchTest.cs
+++ CSI.BatchTracker.Tests/Domain/NativeModels/ReceivedBatchTest.cs
@@ -16,13 +16,14 @@ namespace CSI.BatchTracker.Tests.Domain.NativeModels
         readonly string batchNumber = "872880703201";
         readonly DateTime date = DateTime.Now;
         readonly int qtyAvailable = 5;
+        readonly int poNumber = 40902;
         readonly BatchOperator receivingOperator = new BatchOperator("Jane", "Doe");
         ReceivedBatch batch;
 
         [SetUp]
         public void SetUp()
         {
-            batch = new ReceivedBatch(colorName, batchNumber, date, qtyAvailable, receivingOperator);
+            batch = new ReceivedBatch(colorName, batchNumber, date, qtyAvailable, poNumber, receivingOperator);
         }
 
         [Test]
@@ -32,25 +33,38 @@ namespace CSI.BatchTracker.Tests.Domain.NativeModels
             Assert.AreEqual(batchNumber, batch.BatchNumber);
             Assert.AreEqual(date, batch.ActivityDate);
             Assert.AreEqual(qtyAvailable, batch.Quantity);
+            Assert.AreEqual(poNumber, batch.PONumber);
             Assert.AreSame(receivingOperator, batch.ReceivingOperator);
         }
 
         [Test]
         public void ExceptionIfColorNameIsEmpty()
         {
-            Assert.Throws<BatchException>(() => new ReceivedBatch("", batchNumber, date, qtyAvailable, receivingOperator));
+            Assert.Throws<BatchException>(() => new ReceivedBatch("", batchNumber, date, qtyAvailable, poNumber, receivingOperator));
         }
 
         [Test]
         public void ExceptionIfBatchNumberIsEmpty()
         {
-            Assert.Throws<BatchException>(() => new ReceivedBatch(colorName, "", date, qtyAvailable, receivingOperator));
+            Assert.Throws<BatchException>(() => new ReceivedBatch(colorName, "", date, qtyAvailable, poNumber, receivingOperator));
         }
 
         [Test]
         public void ExceptionIfQuantityIsLessThanOne()
         {
-            Assert.Throws<BatchException>(() => new ReceivedBatch(colorName, batchNumber, date, 0, receivingOperator));
+            Assert.Throws<BatchException>(() => new ReceivedBatch(colorName, batchNumber, date, 0, poNumber, receivingOperator));
+        }
+
+        [Test]
+        public void ExceptionIfPONumberIsNotAtLeastFiveDigits()
+        {
+            Assert.Throws<BatchException>(() => new ReceivedBatch(colorName, batchNumber, date, qtyAvailable, 1111, receivingOperator));
+        }
+
+        [Test]
+        public void PoNumberCanBeBiggerThanFiveDigits()
+        {
+            batch = new ReceivedBatch(colorName, batchNumber, date, qtyAvailable, 111111, receivingOperator);
         }
     }
 }
--- CSI.BatchTracker/Domain/NativeModels/ReceivedBatch.cs
+++ CSI.BatchTracker/Domain/NativeModels/ReceivedBatch.cs
@@ -1,8 +1,10 @@
 ﻿using CSI.BatchTracker.Contracts;
+using CSI.BatchTracker.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CSI.BatchTracker.Domain.NativeModels
@@ -10,25 +12,39 @@ namespace CSI.BatchTracker.Domain.NativeModels
     public class ReceivedBatch : AbstractBatch
     {
         public int Quantity { get; private set; }
+        public int PONumber { get; private set; }
         public BatchOperator ReceivingOperator { get; private set; }
 
         public ReceivedBatch(
             string colorName, 
             string batchNumber, 
             DateTime receivingDate, 
-            int quantity, 
+            int quantity,
+            int poNumber,
             BatchOperator receivingOperator
         )
         {
             CheckIfColorNameIsEmpty(colorName);
             CheckIfBatchNumberIsEmpty(batchNumber);
             CheckIfQuantityIsGreaterThanZero(quantity);
+            CheckIfPONumberIsAtLeastFiveDigits(poNumber);
 
             ColorName = colorName;
             BatchNumber = batchNumber;
             ActivityDate = receivingDate;
             Quantity = quantity;
+            PONumber = poNumber;
             ReceivingOperator = receivingOperator;
+        }
+
+        void CheckIfPONumberIsAtLeastFiveDigits(int poNumber)
+        {
+            Match regex = Regex.Match(poNumber.ToString(), @"^[0-9]{5,}$");
+
+            if (regex.Success == false)
+            {
+                throw new BatchException("PO number must be at least 5 digits.");
+            }
         }
     }
 }
