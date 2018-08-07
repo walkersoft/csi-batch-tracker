49dd044be44b80e8a68b07d9cc7417a89a4f23ef
Jason L. Walker <jasonlanewalker@gmail.com>
2018-08-07 11:33:04 -0500

    created generic entity class

---
+++ CSI.BatchTracker.Tests/Domain/DataSource/EntityTest.cs
@@ -0,0 +1,40 @@
+﻿using CSI.BatchTracker.Domain.DataSource;
+using CSI.BatchTracker.Domain.NativeModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.Domain.DataSource
+{
+    [TestFixture]
+    class EntityTest
+    {
+        Entity<BatchOperator> entity;
+
+        [Test]
+        public void SameNativeModelIsAvailable()
+        {
+            BatchOperator batchOperator = new BatchOperator("Jane", "Doe");
+            entity = new Entity<BatchOperator>(batchOperator);
+
+            Assert.AreSame(batchOperator, entity.NativeModel);
+        }
+
+        [Test]
+        public void SystemIdIsZeroForNewEntity()
+        {
+            entity = new Entity<BatchOperator>(new BatchOperator("Jane", "Doe"));
+            Assert.AreEqual(0, entity.SystemId);
+        }
+
+        [Test]
+        public void CreateEntityWithExistingId()
+        {
+            int systemId = 4;
+            entity = new Entity<BatchOperator>(systemId, new BatchOperator("Jane", "Doe"));
+        }
+    }
+}
---
+++ CSI.BatchTracker/Domain/DataSource/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Domain.DataSource
+{
+    public class Entity<T> where T : class
+    {
+        public int SystemId { get; private set; }
+        public T NativeModel { get; set; }
+
+        public Entity(T nativeModel)
+        {
+            SystemId = 0;
+            NativeModel = nativeModel;
+        }
+
+        public Entity(int systemId, T nativeModel)
+        {
+            SystemId = systemId;
+            NativeModel = nativeModel;
+        }
+    }
+}
