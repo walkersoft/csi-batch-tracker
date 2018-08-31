9ebf937b4fcb2e2df848c974a8bbb4462744b9f7
Jason L. Walker <jasonlanewalker@gmail.com>
2018-08-31 15:54:56 -0500

    working on tests for inventory batch repository

---
+++ CSI.BatchTracker.Tests/Domain/DataSource/InventoryBatchRepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿using CSI.BatchTracker.Contracts;
+using CSI.BatchTracker.Domain.DataSource;
+using CSI.BatchTracker.Domain.DataSource.Repositories;
+using CSI.BatchTracker.Domain.NativeModels;
+using CSI.BatchTracker.Experimental;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.Domain.DataSource
+{
+    [TestFixture]
+    class InventoryBatchRepositoryTest
+    {
+        IRepository<Entity<InventoryBatch>> repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            repository = new InventoryBatchRepository(new DataStore());
+        }
+    }
+}
---
+++ CSI.BatchTracker/Domain/DataSource/Repositories/InventoryBatchRepository.cs
@@ -0,0 +1,48 @@
+﻿using CSI.BatchTracker.Contracts;
+using CSI.BatchTracker.Domain.NativeModels;
+using CSI.BatchTracker.Experimental;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Domain.DataSource.Repositories
+{
+    public class InventoryBatchRepository : IRepository<Entity<InventoryBatch>>
+    {
+        public ObservableCollection<Entity<InventoryBatch>> Items => throw new NotImplementedException();
+        DataStore store;
+
+        public InventoryBatchRepository(DataStore store)
+        {
+            this.store = store;
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entity<InventoryBatch>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entity<InventoryBatch>> FindAll(int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entity<InventoryBatch>> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Entity<InventoryBatch> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
