420c02ad9034355c6ccf0f50117940a07c46014a
Jason L. Walker <jasonlanewalker@gmail.com>
2018-09-27 16:06:04 -0500

    added transaction to rollback a batch that was commited to the ledger by mistake

---
+++ CSI.BatchTracker.Tests/Domain/DataSource/MemoryDataSource/Transactions/InventoryManagement/UndoImplementedBatchCommittedToLedgerTransactionTest.cs
@@ -0,0 +1,131 @@
+﻿using CSI.BatchTracker.DataSource.MemoryDataSource;
+using CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.InventoryManagement;
+using CSI.BatchTracker.Domain.DataSource;
+using CSI.BatchTracker.Domain.NativeModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.Domain.DataSource.MemoryDataSource.Transactions.InventoryManagement
+{
+    [TestFixture]
+    class UndoImplementedBatchCommittedToLedgerTransactionTest
+    {
+        MemoryStore store;
+        LoggedBatch loggedBatch;
+        InventoryBatch inventoryBatch;
+        Entity<LoggedBatch> loggedEntity;
+        Entity<InventoryBatch> inventoryEntity;
+        UndoImplementedBatchCommittedToLedgerTransaction undo;
+        AddReceivedBatchToInventoryTransaction inventoryAdder;
+        AddBatchToImplementedBatchLedgerTransaction inventoryImplementer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            store = new MemoryStore();
+        }
+
+        [Test]
+        public void UndoingBatchRemovesExistingEntryFromImplementationLedger()
+        {
+            int expectedCountBefore = 1;
+            int expectedCountAfter = 0;
+            int targetId = AddBatchToInventoryThenLedger();
+            
+            Assert.AreEqual(expectedCountBefore, store.ImplementedBatchLedger.Count);
+
+            undo = new UndoImplementedBatchCommittedToLedgerTransaction(store.ImplementedBatchLedger[targetId], store);
+            undo.Execute();
+
+            Assert.AreEqual(expectedCountAfter, store.ImplementedBatchLedger.Count);
+        }
+        
+        [Test]
+        public void UndoingBatchRecommitsBatchToInventoryAndAddsEntryIfBatchDoesNotExist()
+        {
+            int expectedCount = 1;
+            int expectedQty = 1;
+            int targetId = AddBatchToInventoryThenLedger();
+
+            undo = new UndoImplementedBatchCommittedToLedgerTransaction(store.ImplementedBatchLedger[targetId], store);
+            undo.Execute();
+
+            Assert.AreEqual(expectedCount, store.CurrentInventory.Count);
+            Assert.AreEqual(expectedQty, store.CurrentInventory[inventoryAdder.LastSystemId].NativeModel.Quantity);
+        }
+
+        [Test]
+        public void UndoingBatchRecommitsBatchToInventoryAndMergesWithExistingBatch()
+        {
+            int expectedQtyBefore = 2;
+            int expectedQtyAfter = 3;
+            int expectedCount = 0;
+            int targetId = AddThreeOfSameBatchToInventoryThenOneToLedger();
+
+            Assert.AreEqual(expectedQtyBefore, store.CurrentInventory[inventoryAdder.LastSystemId].NativeModel.Quantity);
+
+            undo = new UndoImplementedBatchCommittedToLedgerTransaction(store.ImplementedBatchLedger[targetId], store);
+            undo.Execute();
+
+            Assert.AreEqual(expectedQtyAfter, store.CurrentInventory[inventoryAdder.LastSystemId].NativeModel.Quantity);
+            Assert.AreEqual(expectedCount, store.ImplementedBatchLedger.Count);
+        }
+
+        int AddBatchToInventoryThenLedger()
+        {
+            AddBatchToInventory();
+
+            loggedEntity = GetLoggedBatchEntityFromInventoryBatchEntity(store.CurrentInventory[inventoryAdder.LastSystemId]);
+
+            inventoryImplementer = new AddBatchToImplementedBatchLedgerTransaction(loggedEntity, store);
+            inventoryImplementer.Execute();
+
+            return inventoryImplementer.LastSystemId;
+        }
+
+        int AddThreeOfSameBatchToInventoryThenOneToLedger()
+        {
+            InventoryBatch batch = new InventoryBatch("White", "872882210102", DateTime.Now, 3);
+            inventoryEntity = new Entity<InventoryBatch>(batch);
+            inventoryAdder = new AddReceivedBatchToInventoryTransaction(inventoryEntity, store);
+            inventoryAdder.Execute();
+
+            loggedEntity = GetLoggedBatchEntityFromInventoryBatchEntity(inventoryEntity);
+            inventoryImplementer = new AddBatchToImplementedBatchLedgerTransaction(loggedEntity, store);
+            inventoryImplementer.Execute();
+
+            return inventoryImplementer.LastSystemId;
+        }
+
+        void AddBatchToInventory()
+        {
+            inventoryEntity = GetInventoryBatchEntity();
+
+            inventoryAdder = new AddReceivedBatchToInventoryTransaction(inventoryEntity, store);
+            inventoryAdder.Execute();
+        }
+
+        Entity<InventoryBatch> GetInventoryBatchEntity()
+        {
+            InventoryBatch batch = new InventoryBatch("White", "872882210102", DateTime.Now, 1);
+            return new Entity<InventoryBatch>(batch);
+        }
+
+        Entity<LoggedBatch> GetLoggedBatchEntityFromInventoryBatchEntity(Entity<InventoryBatch> entity)
+        {
+            LoggedBatch logged = new LoggedBatch(
+                entity.NativeModel.ColorName,
+                entity.NativeModel.BatchNumber,
+                DateTime.Now,
+                new BatchOperator("Jane", "Doe")
+            );
+
+            return new Entity<LoggedBatch>(logged);
+        }
+
+    }
+}
---
+++ CSI.BatchTracker/DataSource/MemoryDataSource/Transactions/InventoryManagement/UndoImplementedBatchCommittedToLedgerTransaction.cs
@@ -0,0 +1,52 @@
+﻿using CSI.BatchTracker.Domain.DataSource;
+using CSI.BatchTracker.Domain.NativeModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.InventoryManagement
+{
+    public class UndoImplementedBatchCommittedToLedgerTransaction : MemoryDataSourceTransaction
+    {
+        MemoryStore store;
+        Entity<LoggedBatch> loggedEntity;
+        Entity<InventoryBatch> inventoryEntity;
+        AddReceivedBatchToInventoryTransaction inventoryAdder;
+
+        public UndoImplementedBatchCommittedToLedgerTransaction(Entity<LoggedBatch> entity, MemoryStore store)
+        {
+            this.loggedEntity = entity;
+            this.store = store;
+        }
+
+        public override void Execute()
+        {
+            if (store.ImplementedBatchLedger.ContainsKey(loggedEntity.SystemId))
+            {
+                inventoryEntity = RetainLoggedEntityAsInventoryEntity();
+                store.ImplementedBatchLedger.Remove(loggedEntity.SystemId);
+                RecommitToInventory();
+            }
+        }
+
+        Entity<InventoryBatch> RetainLoggedEntityAsInventoryEntity()
+        {
+            return new Entity<InventoryBatch>(
+                new InventoryBatch(
+                    loggedEntity.NativeModel.ColorName,
+                    loggedEntity.NativeModel.BatchNumber,
+                    loggedEntity.NativeModel.ActivityDate,
+                    1
+                )
+            );
+        }
+
+        void RecommitToInventory()
+        {
+            inventoryAdder = new AddReceivedBatchToInventoryTransaction(inventoryEntity, store);
+            inventoryAdder.Execute();
+        }
+    }
+}
