e040e76c13e5fc1f20f8ac9699019801f5f84ca5
Jason L. Walker <jasonlanewalker@gmail.com>
2018-09-28 10:26:59 -0500

    added transaction to edit an batch in inventory

---
+++ CSI.BatchTracker.Tests/DataSource/MemoryDataSource/Transactions/InventoryManagement/EditBatchInCurrentInventoryTransactionTest.cs
@@ -0,0 +1,51 @@
+﻿using CSI.BatchTracker.DataSource.MemoryDataSource;
+using CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.InventoryManagement;
+using CSI.BatchTracker.Domain.DataSource;
+using CSI.BatchTracker.Domain.NativeModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.DataSource.MemoryDataSource.Transactions.InventoryManagement
+{
+    [TestFixture]
+    class EditBatchInCurrentInventoryTransactionTest
+    {
+        MemoryStore store;
+        Entity<InventoryBatch> entity;
+        AddReceivedBatchToInventoryTransaction adder;
+        EditBatchInCurrentInventoryTransaction updater;
+
+        [Test]
+        public void EditingBatchInCurrentInventoryChangesNativeModel()
+        {
+            MemoryStore store = new MemoryStore();
+            adder = new AddReceivedBatchToInventoryTransaction(GetOriginalInventoryEntity(), store);
+            adder.Execute();
+
+            entity = GetUpdatedInventoryEntity(adder.LastSystemId);
+            InventoryBatch expectedBatch = entity.NativeModel;
+
+            updater = new EditBatchInCurrentInventoryTransaction(entity, store);
+            updater.Execute();
+
+            Assert.AreSame(expectedBatch, store.CurrentInventory[entity.SystemId].NativeModel);
+        }
+
+        Entity<InventoryBatch> GetOriginalInventoryEntity()
+        {
+            InventoryBatch batch = new InventoryBatch("White", "872882308203", DateTime.Now, 5);
+            return new Entity<InventoryBatch>(batch);
+        }
+
+        Entity<InventoryBatch> GetUpdatedInventoryEntity(int systemId)
+        {
+            InventoryBatch batch = new InventoryBatch("White", "872882308203", DateTime.Now, 10);
+            return new Entity<InventoryBatch>(systemId, batch);
+        }
+
+    }
+}
---
+++ CSI.BatchTracker/DataSource/MemoryDataSource/Transactions/InventoryManagement/EditBatchInCurrentInventoryTransaction.cs
@@ -0,0 +1,30 @@
+﻿using CSI.BatchTracker.Domain.DataSource;
+using CSI.BatchTracker.Domain.NativeModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.InventoryManagement
+{
+    public sealed class EditBatchInCurrentInventoryTransaction : MemoryDataSourceTransaction
+    {
+        MemoryStore store;
+        Entity<InventoryBatch> entity;
+
+        public EditBatchInCurrentInventoryTransaction(Entity<InventoryBatch> entity, MemoryStore store)
+        {
+            this.entity = entity;
+            this.store = store;
+        }
+
+        public override void Execute()
+        {
+            if (store.CurrentInventory.ContainsKey(entity.SystemId))
+            {
+                store.CurrentInventory[entity.SystemId] = entity;
+            }
+        }
+    }
+}
