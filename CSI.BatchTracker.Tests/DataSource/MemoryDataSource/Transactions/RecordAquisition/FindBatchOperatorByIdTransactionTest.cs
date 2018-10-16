d223befc2b29b9e1f87e3e1f947762eded524f2e
Jason L. Walker <jasonlanewalker@gmail.com>
2018-10-16 13:49:19 -0500

    added transaction to find batch operator by their id

---
+++ CSI.BatchTracker.Tests/DataSource/MemoryDataSource/Transactions/RecordAquisition/FindBatchOperatorByIdTransactionTest.cs
@@ -0,0 +1,60 @@
+﻿using CSI.BatchTracker.DataSource;
+using CSI.BatchTracker.DataSource.MemoryDataSource;
+using CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.BatchOperators;
+using CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.RecordAquisition;
+using CSI.BatchTracker.Domain.DataSource;
+using CSI.BatchTracker.Domain.NativeModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.DataSource.MemoryDataSource.Transactions.RecordAquisition
+{
+    [TestFixture]
+    class FindBatchOperatorByIdTransactionTest
+    {
+        MemoryStore store;
+
+        [SetUp]
+        public void SetUp()
+        {
+            store = new MemoryStore();
+        }
+
+        [Test]
+        public void NonExistantSystemIdProducesNoResults()
+        {
+            int targetId = 1;
+            int expectedQty = 0;
+
+            ITransaction finder = new FindBatchOperatorByIdTransaction(targetId, store);
+
+            Assert.AreEqual(expectedQty, finder.Results.Count);
+        }
+
+        [Test]
+        public void FindBatchOperatorWithExistingSystemId()
+        {
+            int targetId = 1;
+            int expectedQty = 1;
+            PutSingleEntryInBatchOperatorsDataSource();
+
+            ITransaction finder = new FindBatchOperatorByIdTransaction(targetId, store);
+            finder.Execute();
+
+            Assert.AreEqual(expectedQty, finder.Results.Count);
+        }
+
+        void PutSingleEntryInBatchOperatorsDataSource()
+        {
+            BatchOperator batchOperator = new BatchOperator("Jane", "Doe");
+            Entity<BatchOperator> entity = new Entity<BatchOperator>(batchOperator);
+            ITransaction adder = new AddBatchOperatorTransaction(entity, store);
+
+            adder.Execute();
+        }
+    }
+}
---
+++ CSI.BatchTracker/DataSource/MemoryDataSource/Transactions/RecordAquisition/FindBatchOperatorByIdTransaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.RecordAquisition
+{
+    public sealed class FindBatchOperatorByIdTransaction : MemoryDataSourceTransaction
+    {
+        int TargetId { get; set; }
+        MemoryStore store;
+
+        public FindBatchOperatorByIdTransaction(int targetId, MemoryStore store)
+        {
+            TargetId = targetId;
+            this.store = store;
+        }
+
+        public override void Execute()
+        {
+            Results.Clear();
+
+            if (store.BatchOperators.ContainsKey(TargetId))
+            {
+                Results.Add(store.BatchOperators[TargetId]);
+            }
+        }
+    }
+}
