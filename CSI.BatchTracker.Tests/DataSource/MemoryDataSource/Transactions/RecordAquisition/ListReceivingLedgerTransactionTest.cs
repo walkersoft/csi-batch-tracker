84b8951e10fd6fc6c64e2df80e561e224269e29c
Jason L. Walker <jasonlanewalker@gmail.com>
2018-10-26 16:31:54 -0500

    working on listing entries from receiving ledger

---
+++ CSI.BatchTracker.Tests/DataSource/MemoryDataSource/Transactions/RecordAquisition/ListReceivingLedgerTransactionTest.cs
@@ -0,0 +1,35 @@
+﻿using CSI.BatchTracker.DataSource;
+using CSI.BatchTracker.DataSource.MemoryDataSource;
+using CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.RecordAquisition;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.DataSource.MemoryDataSource.Transactions.RecordAquisition
+{
+    [TestFixture]
+    class ListReceivingLedgerTransactionTest
+    {
+        MemoryStore store;
+
+        [Test]
+        public void ListAllReceivedBatchesInReceivingLedger()
+        {
+            int expectedCount = 5;
+            ITransaction finder = new ListReceivingLedgerTransaction(store);
+
+            PopulateDataSourceWithFiveLedgerTransactions();
+            finder.Execute();
+
+            Assert.AreEqual(expectedCount, store);
+        }
+
+        void PopulateDataSourceWithFiveLedgerTransactions()
+        {
+
+        }
+    }
+}
---
+++ CSI.BatchTracker/DataSource/MemoryDataSource/Transactions/RecordAquisition/ListReceivingLedgerTransaction.cs
@@ -0,0 +1,30 @@
+﻿using CSI.BatchTracker.Domain.DataSource;
+using CSI.BatchTracker.Domain.NativeModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.RecordAquisition
+{
+    public sealed class ListReceivingLedgerTransaction : MemoryDataSourceTransaction
+    {
+        MemoryStore store;
+
+        public ListReceivingLedgerTransaction(MemoryStore store)
+        {
+            this.store = store;
+        }
+
+        public override void Execute()
+        {
+            Results.Clear();
+
+            foreach (KeyValuePair<int, Entity<ReceivedBatch>> entity in store.ReceivingLedger)
+            {
+                Results.Add(entity.Value);
+            }
+        }
+    }
+}
