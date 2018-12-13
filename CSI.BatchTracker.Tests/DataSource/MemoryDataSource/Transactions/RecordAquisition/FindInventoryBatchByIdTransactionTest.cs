81144fd31cf3da755bf25bd0f71f4418192fb638
Jason L. Walker <jasonlanewalker@gmail.com>
2018-12-13 15:39:51 -0600

    added support to find specific inventory entity at a given identifier

---
+++ CSI.BatchTracker.Tests/DataSource/MemoryDataSource/Transactions/RecordAquisition/FindInventoryBatchByIdTransactionTest.cs
@@ -0,0 +1,56 @@
+﻿using CSI.BatchTracker.DataSource;
+using CSI.BatchTracker.DataSource.MemoryDataSource;
+using CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.InventoryManagement;
+using CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.RecordAquisition;
+using CSI.BatchTracker.Domain.DataSource;
+using CSI.BatchTracker.Domain.NativeModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.DataSource.MemoryDataSource.Transactions.RecordAquisition
+{
+    [TestFixture]
+    class FindInventoryBatchByIdTransactionTest
+    {
+        MemoryStore store;
+
+        [Test]
+        public void FindEntityInInventory()
+        {
+            int targetId = 3;
+            store = new MemoryStore();
+            ITransaction finder = new FindInventoryBatchByIdTransaction(targetId, store);
+
+            PutThreeBatchesInInventory();
+            finder.Execute();
+
+            Assert.AreEqual(targetId, finder.Results[0].SystemId);
+        }
+
+        void PutThreeBatchesInInventory()
+        {
+            InventoryBatch batch1 = new InventoryBatch("White", "872881801202", DateTime.Now, 3);
+            InventoryBatch batch2 = new InventoryBatch("White", "872881801502", DateTime.Now, 3);
+            InventoryBatch batch3 = new InventoryBatch("White", "872881801208", DateTime.Now, 3);
+
+            Entity<InventoryBatch> entity;
+            ITransaction adder;
+
+            entity = new Entity<InventoryBatch>(batch1);
+            adder = new AddReceivedBatchToInventoryTransaction(entity, store);
+            adder.Execute();
+
+            entity = new Entity<InventoryBatch>(batch2);
+            adder = new AddReceivedBatchToInventoryTransaction(entity, store);
+            adder.Execute();
+
+            entity = new Entity<InventoryBatch>(batch3);
+            adder = new AddReceivedBatchToInventoryTransaction(entity, store);
+            adder.Execute();
+        }
+    }
+}
---
+++ CSI.BatchTracker/DataSource/MemoryDataSource/Transactions/RecordAquisition/FindInventoryBatchByIdTransaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.DataSource.MemoryDataSource.Transactions.RecordAquisition
+{
+    public sealed class FindInventoryBatchByIdTransaction : MemoryDataSourceTransaction
+    {
+        int TargetId { get; set; }
+        MemoryStore store;
+
+        public FindInventoryBatchByIdTransaction(int targetId, MemoryStore store)
+        {
+            TargetId = targetId;
+            this.store = store;
+        }
+
+        public override void Execute()
+        {
+            Results.Clear();
+
+            if (store.CurrentInventory.ContainsKey(TargetId))
+            {
+                Results.Add(store.CurrentInventory[TargetId]);
+            }
+        }
+    }
+}
