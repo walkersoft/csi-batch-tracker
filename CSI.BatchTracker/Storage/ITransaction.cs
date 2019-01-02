f4e606a06c872e66cc149c837076792bbb84eb35
Jason L. Walker <jasonlanewalker@gmail.com>
2019-01-01 20:09:30 -0600

    changed namespace for ITransaction

--- CSI.BatchTracker/Storage/ITransaction.cs
+++ CSI.BatchTracker/Storage/ITransaction.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSI.BatchTracker.Storage
+namespace CSI.BatchTracker.Storage.Contracts
 {
     public interface ITransaction
     {
