f756bbb80e215fe0f3df52206cad45951a912079
Jason L. Walker <jasonlanewalker@gmail.com>
2019-06-03 09:36:58 -0500

    refactored latest tests to abstract what the test is doing from implementations

---
+++ CSI.BatchTracker.Tests/ViewModels/Commands/Behaviors/ListReceivingRecordsByDateRangeCommandBehaviorTest.cs
@@ -0,0 +1,80 @@
+﻿using CSI.BatchTracker.Domain.NativeModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.ViewModels.Commands.Behaviors
+{
+    [TestFixture]
+    abstract class ListReceivingRecordsByDateRangeCommandBehaviorTest : ReceivingHistoryViewModelCommandBehaviorTestingBase
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        [Test]
+        public void DefaultCommandSetupWillExecute()
+        {
+            Assert.True(command.CanExecute(null));
+        }
+
+        [Test]
+        public void CommandWillNotExecuteIfStartingDateIsAfterEndingDateAndADifferentDay()
+        {
+            viewModel.DateRangeEndingDate = DateTime.Now;
+            viewModel.DateRangeStartingDate = viewModel.DateRangeEndingDate.AddDays(1);
+
+            Assert.False(command.CanExecute(null));
+        }
+
+        [Test]
+        public void CommandWillExecuteIfEndingDateIsBeforeStartingDateButIsStillTheSameDay()
+        {
+            viewModel.DateRangeStartingDate = DateTime.Today.AddHours(10);
+            viewModel.DateRangeEndingDate = viewModel.DateRangeStartingDate.AddHours(-2);
+
+            Assert.True(command.CanExecute(null));
+        }
+
+        [Test]
+        public void CommandWillExecuteIfStartingDateIsBeforeEndingDateAndNotTheSameDay()
+        {
+            viewModel.DateRangeStartingDate = DateTime.Today;
+            viewModel.DateRangeEndingDate = viewModel.DateRangeStartingDate.AddDays(5);
+
+            Assert.True(command.CanExecute(null));
+        }
+
+        [Test]
+        public void ExecutedCommandWillPopulateRetreivedRecordsLedger()
+        {
+            int expectedCount = 1;
+            viewModel.DateRangeStartingDate = DateTime.Today;
+            viewModel.DateRangeEndingDate = viewModel.DateRangeStartingDate.AddDays(3);
+            DateTime firstItemDate = viewModel.DateRangeStartingDate.AddDays(1);
+
+            List<ReceivedBatch> batches = new List<ReceivedBatch>()
+            {
+                helper.GetBatchWithSpecificDate(viewModel.DateRangeStartingDate.AddDays(-1)),
+                helper.GetBatchWithSpecificDate(firstItemDate),
+                helper.GetBatchWithSpecificDate(viewModel.DateRangeStartingDate.AddDays(2)),
+                helper.GetBatchWithSpecificDate(viewModel.DateRangeEndingDate.AddDays(1))
+            };
+
+            foreach (ReceivedBatch batch in batches)
+            {
+                receivedBatchSource.SaveReceivedBatch(batch);
+            }
+
+            command.Execute(null);
+
+            Assert.AreEqual(expectedCount, viewModel.RetreivedRecordsLedger.Count);
+            Assert.AreEqual(firstItemDate, viewModel.RetreivedRecordsLedger[0].ActivityDate);
+        }
+    }
+}
--- CSI.BatchTracker.Tests/ViewModels/Commands/WithMemoryStore/ListReceivingRecordsByDateRangeCommandTest.cs
+++ CSI.BatchTracker.Tests/ViewModels/Commands/WithMemoryStore/ListReceivingRecordsByDateRangeCommandTest.cs
@@ -15,7 +15,7 @@ using System.Threading.Tasks;
 namespace CSI.BatchTracker.Tests.ViewModels.Commands.WithMemoryStore
 {
     [TestFixture]
-    class ListReceivingRecordsByDateRangeCommandTest : ReceivingHistoryViewModelCommandBehaviorTestingBase
+    class ListReceivingRecordsByDateRangeCommandTest : ListReceivingRecordsByDateRangeCommandBehaviorTest
     {
         [SetUp]
         public override void SetUp()
@@ -26,66 +26,6 @@ namespace CSI.BatchTracker.Tests.ViewModels.Commands.WithMemoryStore
             viewModel = new ReceivingHistoryViewModel(receivedBatchSource, inventorySource);
             command = new ListReceivingRecordsByDateRangeCommand(viewModel);
             base.SetUp();
-        }
-
-        [Test]
-        public void DefaultCommandSetupWillExecute()
-        {
-            Assert.True(command.CanExecute(null));
-        }
-
-        [Test]
-        public void CommandWillNotExecuteIfStartingDateIsAfterEndingDateAndADifferentDay()
-        {
-            viewModel.DateRangeEndingDate = DateTime.Now;
-            viewModel.DateRangeStartingDate = viewModel.DateRangeEndingDate.AddDays(1);
-
-            Assert.False(command.CanExecute(null));
-        }
-        
-        [Test]
-        public void CommandWillExecuteIfEndingDateIsBeforeStartingDateButIsStillTheSameDay()
-        {
-            viewModel.DateRangeStartingDate = DateTime.Today.AddHours(10);
-            viewModel.DateRangeEndingDate = viewModel.DateRangeStartingDate.AddHours(-2);
-
-            Assert.True(command.CanExecute(null));
-        }
-
-        [Test]
-        public void CommandWillExecuteIfStartingDateIsBeforeEndingDateAndNotTheSameDay()
-        {
-            viewModel.DateRangeStartingDate = DateTime.Today;
-            viewModel.DateRangeEndingDate = viewModel.DateRangeStartingDate.AddDays(5);
-
-            Assert.True(command.CanExecute(null));
-        }
-
-        [Test]
-        public void ExecutedCommandWillPopulateRetreivedRecordsLedger()
-        {
-            int expectedCount = 1;
-            viewModel.DateRangeStartingDate = DateTime.Today;
-            viewModel.DateRangeEndingDate = viewModel.DateRangeStartingDate.AddDays(3);
-            DateTime firstItemDate = viewModel.DateRangeStartingDate.AddDays(1);
-
-            List<ReceivedBatch> batches = new List<ReceivedBatch>()
-            {
-                helper.GetBatchWithSpecificDate(viewModel.DateRangeStartingDate.AddDays(-1)),
-                helper.GetBatchWithSpecificDate(firstItemDate),
-                helper.GetBatchWithSpecificDate(viewModel.DateRangeStartingDate.AddDays(2)),
-                helper.GetBatchWithSpecificDate(viewModel.DateRangeEndingDate.AddDays(1))
-            };
-
-            foreach (ReceivedBatch batch in batches)
-            {
-                receivedBatchSource.SaveReceivedBatch(batch);
-            }
-
-            command.Execute(null);
-
-            Assert.AreEqual(expectedCount, viewModel.RetreivedRecordsLedger.Count);
-            Assert.AreEqual(firstItemDate, viewModel.RetreivedRecordsLedger[0].ActivityDate);
         }
     }
 }
