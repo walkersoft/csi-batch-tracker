71fa8e543ba6bb055aa6a6dc4a5dbd199ab0cd27
Jason L. Walker <jasonlanewalker@gmail.com>
2019-07-09 17:05:49 -0500

    working on validation protocols for changed purchase orders

--- CSI.BatchTracker.Tests/Domain/ModifiedPurchaseOrderValidatorBehaviorTest.cs
+++ CSI.BatchTracker.Tests/Domain/ModifiedPurchaseOrderValidatorBehaviorTest.cs
@@ -1,10 +1,12 @@
 ﻿using CSI.BatchTracker.Domain;
 using CSI.BatchTracker.Domain.DataSource.Contracts;
 using CSI.BatchTracker.Domain.NativeModels;
+using CSI.BatchTracker.Exceptions;
 using CSI.BatchTracker.Tests.TestHelpers.NativeModels;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -69,7 +71,11 @@ namespace CSI.BatchTracker.Tests.Domain
 
         void ImportReceivingRecordsIntoValidator()
         {
-            modified = new ModifiedPurchaseOrderValidator(receivedBatchSource.GetReceivedBatchesByPONumber(11111));
+            modified = new ModifiedPurchaseOrderValidator(
+                receivedBatchSource.GetReceivedBatchesByPONumber(11111),
+                inventorySource,
+                implementedBatchSource
+            );
         }
 
         [Test]
@@ -82,6 +88,55 @@ namespace CSI.BatchTracker.Tests.Domain
             Assert.AreEqual(whiteBatch, modified.ReceivedBatches[0].BatchNumber);
             Assert.AreEqual(blackBatch, modified.ReceivedBatches[1].BatchNumber);
             Assert.AreEqual(yellowBatch, modified.ReceivedBatches[2].BatchNumber);
+        }
+
+        [Test]
+        public void ExceptionIsThrownIfBatchRecordSetIsEmpty()
+        {
+            Assert.Throws<ModifiedPurchaseOrderValidationException>(
+                () => new ModifiedPurchaseOrderValidator(new ObservableCollection<ReceivedBatch>(), inventorySource, implementedBatchSource)
+            );
+        }
+
+        [Test]
+        public void ReceivedBatchCanBeDeletedIfItHasNotBeenImplemented()
+        {
+            int expectedRemainingCount = 2;
+            int expectedDeleteCount = 1;
+
+            modified.ReceivedBatches.RemoveAt(1);
+            modified.RunPurchaseOrderComparison();
+
+            Assert.AreEqual(expectedRemainingCount, modified.ReceivedBatches.Count);
+            Assert.AreEqual(expectedDeleteCount, modified.RecordsToDelete.Count);
+        }
+
+        [Test]
+        public void ReceivedBatchCannotBeDeletedIfItHasBeenImplemented()
+        {
+            int expectedRemainingCount = 3;
+            int expectedDeleteCount = 0;
+
+            implementedBatchSource.AddBatchToImplementationLedger(blackBatch, DateTime.Now, operatorSource.FindBatchOperator(originalBatchOperatorId));
+            modified.RunPurchaseOrderComparison();
+
+            Assert.AreEqual(expectedRemainingCount, modified.ReceivedBatches.Count);
+            Assert.AreEqual(expectedDeleteCount, modified.RecordsToDelete.Count);
+        }
+
+        [Test]
+        public void ReceivedBatchesCanHavePONumberUpdated()
+        {
+            int expectedPONumber = 22222;
+            int expectedUpdateCount = 3;
+
+            modified.PONumber = expectedPONumber;
+            modified.RunPurchaseOrderComparison();
+
+            Assert.AreEqual(expectedPONumber, modified.RecordsToUpdate[0].PONumber);
+            Assert.AreEqual(expectedPONumber, modified.RecordsToUpdate[1].PONumber);
+            Assert.AreEqual(expectedPONumber, modified.RecordsToUpdate[2].PONumber);
+            Assert.AreEqual(expectedUpdateCount, modified.RecordsToUpdate.Count);
         }
     }
 }
--- CSI.BatchTracker/Domain/ModifiedPurchaseOrderValidator.cs
+++ CSI.BatchTracker/Domain/ModifiedPurchaseOrderValidator.cs
@@ -1,4 +1,5 @@
-﻿using CSI.BatchTracker.Domain.NativeModels;
+﻿using CSI.BatchTracker.Domain.DataSource.Contracts;
+using CSI.BatchTracker.Domain.NativeModels;
 using CSI.BatchTracker.Exceptions;
 using System;
 using System.Collections.Generic;
@@ -11,14 +12,21 @@ namespace CSI.BatchTracker.Domain
 {
     public sealed class ModifiedPurchaseOrderValidator
     {
-        public int PONumber { get; private set; }
+        public int PONumber { get; set; }
         public DateTime ReceivedDate { get; private set; }
         public BatchOperator ReceivingOperator { get; private set; }
         public ObservableCollection<ReceivedBatch> ReceivedBatches { get; private set; }
+        public List<ReceivedBatch> RecordsToDelete { get; private set; }
+        public List<ReceivedBatch> RecordsToUpdate { get; private set; }
 
         ReceivedPurchaseOrder originalPurchaseOrder;
+        IActiveInventorySource inventorySource;
+        IImplementedBatchSource implementedBatchSource;
 
-        public ModifiedPurchaseOrderValidator(ObservableCollection<ReceivedBatch> receivedBatches)
+        public ModifiedPurchaseOrderValidator(
+            ObservableCollection<ReceivedBatch> receivedBatches,
+            IActiveInventorySource inventorySource,
+            IImplementedBatchSource implementedBatchSource)
         {
             if (receivedBatches.Count < 1)
             {
@@ -26,15 +34,70 @@ namespace CSI.BatchTracker.Domain
                 throw new ModifiedPurchaseOrderValidationException(message);
             }
 
-            ReceivedBatches = receivedBatches;
-            InitializeOriginalPurchaseOrder();
-        }
-
-        void InitializeOriginalPurchaseOrder()
-        {
+            this.inventorySource = inventorySource;
+            this.implementedBatchSource = implementedBatchSource;
+            RecordsToDelete = new List<ReceivedBatch>();
+            RecordsToUpdate = new List<ReceivedBatch>();
+            ReceivedBatches = new ObservableCollection<ReceivedBatch>(receivedBatches);
             PONumber = ReceivedBatches[0].PONumber;
             ReceivedDate = ReceivedBatches[0].ActivityDate;
             ReceivingOperator = ReceivedBatches[0].ReceivingOperator;
+            originalPurchaseOrder = new ReceivedPurchaseOrder(PONumber, ReceivedDate, ReceivingOperator, receivedBatches);
+        }
+
+        public void RunPurchaseOrderComparison()
+        {
+            CheckForUpdatedPONumber();
+            CheckAndValidateDeletedRecords();
+        }
+
+        void CheckForUpdatedPONumber()
+        {
+            if (originalPurchaseOrder.PONumber != PONumber)
+            {
+                RecordsToUpdate.Clear();
+
+                foreach (ReceivedBatch batch in ReceivedBatches)
+                {
+                    batch.PONumber = PONumber;
+                    RecordsToUpdate.Add(batch);
+                }
+            }
+        }
+
+        void CheckAndValidateDeletedRecords()
+        { 
+            foreach (ReceivedBatch original in originalPurchaseOrder.ReceivedBatches)
+            {
+                if (BatchExistsInImplementationLedger(original))
+                {
+                    continue;
+                }              
+
+                if (RecordShouldBeDeleted(original))
+                {
+                    RecordsToDelete.Add(original);
+                }
+            }
+        }
+
+        bool RecordShouldBeDeleted(ReceivedBatch original)
+        {
+            foreach (ReceivedBatch batch in ReceivedBatches)
+            {
+                if (batch.BatchNumber == original.BatchNumber)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        bool BatchExistsInImplementationLedger(ReceivedBatch batch)
+        {
+            ObservableCollection<LoggedBatch> batches = implementedBatchSource.GetImplementedBatchesByBatchNumber(batch.BatchNumber);
+            return batches.Count > 0;
         }
     }
 }
