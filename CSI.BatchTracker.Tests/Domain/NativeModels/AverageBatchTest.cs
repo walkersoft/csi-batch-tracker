24bafd43776745140e8c262e6e109cf9015bff94
Jason L. Walker <jasonlanewalker@gmail.com>
2019-11-18 10:03:35 -0600

    working on average batch testing

---
+++ CSI.BatchTracker.Tests/Domain/NativeModels/AverageBatchTest.cs
@@ -0,0 +1,47 @@
+﻿using CSI.BatchTracker.Domain.NativeModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.Domain.NativeModels
+{
+    [TestFixture]
+    class AverageBatchTest
+    {
+        [Test]
+        public void SettingUpAverageBatchAndGettingAnAverage()
+        {
+            string expectedAverage = "5.00";
+            int productionDays = 20;
+            int quantityUsed = 100;
+            AverageBatch batch = new AverageBatch("White", productionDays, quantityUsed);
+
+            Assert.AreEqual(expectedAverage, batch.DisplayUsage);
+        }
+
+        [Test]
+        public void GettingAverageWithPrecisionThatRoundsSecondDecimalUp()
+        {
+            string expectedAverage = "0.06";
+            int productionDays = 18;
+            int quantityUsed = 1;
+            AverageBatch batch = new AverageBatch("White", productionDays, quantityUsed);
+
+            Assert.AreEqual(expectedAverage, batch.DisplayUsage);
+        }
+
+        [Test]
+        public void GettingAverageWithPrecisionThatRoundsSecondDecimalDown()
+        {
+            string expectedAverage = "0.05";
+            int productionDays = 19;
+            int quantityUsed = 1;
+            AverageBatch batch = new AverageBatch("White", productionDays, quantityUsed);
+
+            Assert.AreEqual(expectedAverage, batch.DisplayUsage);
+        }
+    }
+}
---
+++ CSI.BatchTracker/Domain/NativeModels/AverageBatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Domain.NativeModels
+{
+    public class AverageBatch
+    {
+        public string ColorName { get; private set; }
+        public int ProductionDays { get; private set; }
+        public int QuantityUsed { get; set; }
+        public float AverageUsage
+        {
+            get
+            {
+                return (float)QuantityUsed / (float)ProductionDays;
+            }
+        }
+
+        public string DisplayUsage
+        {
+            get
+            {
+                return AverageUsage.ToString("0.00");
+            }
+        }
+
+        public AverageBatch(string colorName, int productionDays, int quantityUsed = 0)
+        {
+            ColorName = colorName;
+            ProductionDays = productionDays;
+            QuantityUsed = quantityUsed;
+        }
+    }
+}
