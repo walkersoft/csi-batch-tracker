74ca59fd0b0849f2512fd34473fd528bb0e99ecd
Jason L. Walker <jasonlanewalker@gmail.com>
2019-11-25 16:40:37 -0600

    tested creating batch operators table

---
+++ CSI.BatchTracker.Tests/Storage/SQLiteStore/SQLiteDatabaseInstallerTest.cs
@@ -0,0 +1,87 @@
+﻿using CSI.BatchTracker.Storage;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Tests.Storage.SQLiteStore
+{
+    [TestFixture]
+    class SQLiteDatabaseInstallerTest
+    {
+        SQLiteDatabaseInstaller installer;
+        string temporaryDatabaseFile;
+
+        [SetUp]
+        public void SetUp()
+        {
+            temporaryDatabaseFile = Path.GetTempPath() + "temp_database.sqlite3";
+            installer = new SQLiteDatabaseInstaller
+            {
+                DatabaseFile = temporaryDatabaseFile,
+                ConnectionString = string.Format("Data Source={0};Version=3;New=True;", temporaryDatabaseFile)
+            };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (File.Exists(temporaryDatabaseFile))
+            {
+                File.Delete(temporaryDatabaseFile);
+            }
+        }
+
+        [Test]
+        public void DatabaseInstallerCanCreatePhysicalDatabase()
+        {
+            Assert.DoesNotThrow(() => installer.CreateNewDatabase());
+            Assert.True(File.Exists(temporaryDatabaseFile));
+        }
+
+        [Test]
+        public void DatabaseInstallerWillNotCreatePhysicalDatabaseWithoutFileName()
+        {
+            installer.DatabaseFile = string.Empty;
+
+            Assert.DoesNotThrow(() => installer.CreateNewDatabase());
+            Assert.False(File.Exists(temporaryDatabaseFile));
+        }
+
+        [Test]
+        public void NewDatabaseContainsTableForBatchOperators()
+        {
+            int expectedRowCount = 3;
+
+            Assert.DoesNotThrow(() => installer.CreateNewDatabase());
+
+            using (SQLiteConnection connection = new SQLiteConnection(installer.ConnectionString))
+            {
+                connection.Open();
+                string query = "PRAGMA table_info(BatchOperators)";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        Assert.True(reader.HasRows);
+                        int rows = 0;
+
+                        while (reader.Read())
+                        {
+                            rows++;
+                        }
+
+                        Assert.AreEqual(expectedRowCount, rows);
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ CSI.BatchTracker/Storage/SQLiteDatabaseInstaller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI.BatchTracker.Storage
+{
+    public class SQLiteDatabaseInstaller
+    {
+        public string DatabaseFile { get; set; }
+        public string ConnectionString { get; set; }
+
+        public void CreateNewDatabase()
+        {
+            if (string.IsNullOrEmpty(DatabaseFile) == false)
+            {
+                SQLiteConnection.CreateFile(DatabaseFile);
+                CreateBatchOperatorsTable();
+            }
+        }
+
+        void CreateBatchOperatorsTable()
+        {
+            string query = @"
+                CREATE TABLE BatchOperators(
+                    SystemId  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                    FirstName TEXT NOT NULL,
+                    LastName  TEXT NOT NULL
+                )
+            ";
+
+            using (SQLiteConnection connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (SQLiteCommand command = new SQLiteCommand(query.Trim(), connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+    }
+}
