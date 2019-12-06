1480b8df13d2bc45943657d7e1490120b1a6ba6d
Jason L. Walker <jasonlanewalker@gmail.com>
2019-12-06 16:22:14 -0600

    tested and created blank database installer

--- CSI.BatchTracker.Tests/Storage/SQLiteStore/SQLiteDatabaseInstallerTest.cs
+++ CSI.BatchTracker.Tests/Storage/SQLiteStore/SQLiteDatabaseInstallerTest.cs
@@ -83,5 +83,101 @@ namespace CSI.BatchTracker.Tests.Storage.SQLiteStore
                 connection.Close();
             }
         }
+
+        [Test]
+        public void NewDatabaseContainsTableForReceivedBatches()
+        {
+            int expectedRowCount = 7;
+
+            Assert.DoesNotThrow(() => installer.CreateNewDatabase());
+
+            using (SQLiteConnection connection = new SQLiteConnection(installer.ConnectionString))
+            {
+                connection.Open();
+                string query = "PRAGMA table_info(ReceivedBatches)";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        Assert.True(reader.HasRows);
+                        int rows = 0;
+
+                        while (reader.Read())
+                        {
+                            rows++;
+                        }
+
+                        Assert.AreEqual(expectedRowCount, rows);
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+
+        [Test]
+        public void NewDatabaseContainsTableForInventoryBatches()
+        {
+            int expectedRowCount = 5;
+
+            Assert.DoesNotThrow(() => installer.CreateNewDatabase());
+
+            using (SQLiteConnection connection = new SQLiteConnection(installer.ConnectionString))
+            {
+                connection.Open();
+                string query = "PRAGMA table_info(InventoryBatches)";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        Assert.True(reader.HasRows);
+                        int rows = 0;
+
+                        while (reader.Read())
+                        {
+                            rows++;
+                        }
+
+                        Assert.AreEqual(expectedRowCount, rows);
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+
+        [Test]
+        public void NewDatabaseContainsTableForImplementedBatches()
+        {
+            int expectedRowCount = 5;
+
+            Assert.DoesNotThrow(() => installer.CreateNewDatabase());
+
+            using (SQLiteConnection connection = new SQLiteConnection(installer.ConnectionString))
+            {
+                connection.Open();
+                string query = "PRAGMA table_info(ImplementedBatches)";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        Assert.True(reader.HasRows);
+                        int rows = 0;
+
+                        while (reader.Read())
+                        {
+                            rows++;
+                        }
+
+                        Assert.AreEqual(expectedRowCount, rows);
+                    }
+                }
+
+                connection.Close();
+            }
+        }
     }
 }
--- CSI.BatchTracker/Storage/SQLiteDatabaseInstaller.cs
+++ CSI.BatchTracker/Storage/SQLiteDatabaseInstaller.cs
@@ -12,33 +12,76 @@ namespace CSI.BatchTracker.Storage
         public string DatabaseFile { get; set; }
         public string ConnectionString { get; set; }
 
+        string[] tableSchemas = new string[4];
+
         public void CreateNewDatabase()
         {
             if (string.IsNullOrEmpty(DatabaseFile) == false)
             {
                 SQLiteConnection.CreateFile(DatabaseFile);
-                CreateBatchOperatorsTable();
+                CreateTableSchemas();
+                ExecuteTableSchemaQueries();
             }
         }
 
-        void CreateBatchOperatorsTable()
+        void CreateTableSchemas()
         {
-            string query = @"
+            tableSchemas[0] = @"
                 CREATE TABLE BatchOperators(
                     SystemId  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
                     FirstName TEXT NOT NULL,
-                    LastName  TEXT NOT NULL
+                    LastName TEXT NOT NULL
                 )
             ";
 
+            tableSchemas[1] = @"
+                CREATE TABLE ReceivedBatches(
+                    SystemId  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                    ColorName TEXT NOT NULL,
+                    BatchNumber TEXT NOT NULL,
+                    ReceivingDate TEXT NOT NULL,
+                    QtyReceived INTEGER NOT NULL,
+                    PONumber INTEGER NOT NULL,
+                    ReceivingOperatorId INTEGER NOT NULL,
+                    FOREIGN KEY(ReceivingOperatorId) REFERENCES BatchOperators(SystemId)
+                )
+            ";
+
+            tableSchemas[2] = @"
+                CREATE TABLE InventoryBatches(
+                    SystemId  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                    ColorName TEXT NOT NULL,
+                    BatchNumber TEXT NOT NULL,
+                    ActivityDate TEXT NOT NULL,
+                    QtyOnHand INTEGER NOT NULL
+                )
+            ";
+
+            tableSchemas[3] = @"
+                CREATE TABLE ImplementedBatches(
+                    SystemId  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                    ColorName TEXT NOT NULL,
+                    BatchNumber TEXT NOT NULL,
+                    ImplementationDate TEXT NOT NULL,
+                    ImplementingOperatorId INTEGER NOT NULL,
+                    FOREIGN KEY(ImplementingOperatorId) REFERENCES BatchOperators(SystemId)
+                )
+            ";
+        }
+
+        void ExecuteTableSchemaQueries()
+        {
             using (SQLiteConnection connection = new SQLiteConnection(ConnectionString))
             {
                 connection.Open();
 
-                using (SQLiteCommand command = new SQLiteCommand(query.Trim(), connection))
+                for (int i = 0; i < tableSchemas.Length; i++)
                 {
-                    command.ExecuteNonQuery();
-                }
+                    using (SQLiteCommand command = new SQLiteCommand(tableSchemas[i].Trim(), connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }                    
 
                 connection.Close();
             }
