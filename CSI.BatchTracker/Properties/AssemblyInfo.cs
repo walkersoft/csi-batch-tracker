80fe36ede6a473103e1503c5b30ee8312a2ca95d
Jason L. Walker <jasonlanewalker@gmail.com>
2020-03-10 13:52:55 -0500

    bump to v0.4 in assembly

--- CSI.BatchTracker/Properties/AssemblyInfo.cs
+++ CSI.BatchTracker/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BatchTRAX")]
-[assembly: AssemblyDescription("Pigment batch tracking management and analysis software suite")]
+[assembly: AssemblyDescription("Pigment batch tracking management and analysis software suite.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cedar Siding, Inc.")]
 [assembly: AssemblyProduct("BatchTRAX")]
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
