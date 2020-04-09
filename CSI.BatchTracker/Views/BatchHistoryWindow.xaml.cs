fa0465c98546888a4f82022c8783a624a699063b
Jason L. Walker <jasonlanewalker@gmail.com>
2020-04-09 07:32:05 -0500

    updated batch history to metro

--- CSI.BatchTracker/Views/BatchHistoryWindow.xaml.cs
+++ CSI.BatchTracker/Views/BatchHistoryWindow.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using CSI.BatchTracker.ViewModels;
+using MahApps.Metro.Controls;
 using System.Windows;
 
 namespace CSI.BatchTracker.Views
 {
-    public partial class BatchHistoryWindow : Window
+    public partial class BatchHistoryWindow : MetroWindow
     {
         public BatchHistoryWindow(BatchHistoryViewModel viewModel)
         {
