7f835b265b0236104e30c2b001276e63d1e820ef
Jason L. Walker <jasonlanewalker@gmail.com>
2020-04-09 09:52:51 -0500

    updated receiving history to metro

--- CSI.BatchTracker/Views/ReceivingHistoryWindow.xaml.cs
+++ CSI.BatchTracker/Views/ReceivingHistoryWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CSI.BatchTracker.ViewModels;
+using MahApps.Metro.Controls;
 using System.Windows;
 
 namespace CSI.BatchTracker.Views
@@ -6,7 +7,7 @@ namespace CSI.BatchTracker.Views
     /// <summary>
     /// Interaction logic for ReceivingHistoryWindow.xaml
     /// </summary>
-    public partial class ReceivingHistoryWindow : Window
+    public partial class ReceivingHistoryWindow : MetroWindow
     {
         public ReceivingHistoryWindow(ReceivingHistoryViewModel viewModel)
         {
