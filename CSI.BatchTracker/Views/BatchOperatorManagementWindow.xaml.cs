b8f060bd74f4601cd080da2af9266203990e6184
Jason L. Walker <jasonlanewalker@gmail.com>
2020-04-09 08:02:27 -0500

    updated batch operator to metro

--- CSI.BatchTracker/Views/BatchOperatorManagementWindow.xaml.cs
+++ CSI.BatchTracker/Views/BatchOperatorManagementWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CSI.BatchTracker.ViewModels;
+using MahApps.Metro.Controls;
 using System.Windows;
 
 namespace CSI.BatchTracker.Views
@@ -6,7 +7,7 @@ namespace CSI.BatchTracker.Views
     /// <summary>
     /// Interaction logic for OperatorManagementWindowView.xaml
     /// </summary>
-    public partial class BatchOperatorManagementWindow : Window
+    public partial class BatchOperatorManagementWindow : MetroWindow
     {
         public BatchOperatorManagementWindow(BatchOperatorViewModel viewModel)
         {
