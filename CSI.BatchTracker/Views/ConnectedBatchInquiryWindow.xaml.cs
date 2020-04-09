f84e97ae59a57f10ba6d2da7718e40dea7a55c00
Jason L. Walker <jasonlanewalker@gmail.com>
2020-04-09 09:31:16 -0500

    updated batch inquiry to metro

--- CSI.BatchTracker/Views/ConnectedBatchInquiryWindow.xaml.cs
+++ CSI.BatchTracker/Views/ConnectedBatchInquiryWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CSI.BatchTracker.ViewModels;
+using MahApps.Metro.Controls;
 using System.Windows;
 
 namespace CSI.BatchTracker.Views
@@ -6,7 +7,7 @@ namespace CSI.BatchTracker.Views
     /// <summary>
     /// Interaction logic for ConnectedBatchInquiryWindow.xaml
     /// </summary>
-    public partial class ConnectedBatchInquiryWindow : Window
+    public partial class ConnectedBatchInquiryWindow : MetroWindow
     {
         public ConnectedBatchInquiryWindow(ImplementationInquiryViewModel viewModel)
         {
